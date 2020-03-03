c881c9721a8cf5097daaeac0c799bf03671314f0
tonousa <tonousa@hotmail.com>
2020-03-02 18:15:18 -0600

    encrypt

---
+++ jsTemplating/Encrypt.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Encrypt.aspx.cs" Inherits="jsTemplating.Encrypt" EnableViewState="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="myVal" runat="server"></asp:TextBox>
+        <asp:Button runat="server" Text="Encrypt" ID="EncryptBtn" OnClick="EncryptBtn_Click"  />
+        <asp:Image ID="txtImage" runat="server" Visible="false" />
+    </div>
+    <div>
+        <asp:TextBox ID="decryptTextBox" runat="server"></asp:TextBox>
+        <asp:Button runat="server" Text="Decrypt" ID="DecryptBtn" OnClick="DecryptBtn_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ jsTemplating/Helpers/Helpers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Security;
+
+namespace jsTemplating.Helpers
+{
+    public static class Helpers
+    {
+        public static string EncryptData(string value)
+        {
+            return value + " encrypted";
+        }
+
+        public static string Encrypt(string plaintextValue)
+        {
+            var plaintextBytes = Encoding.UTF8.GetBytes(plaintextValue);
+            return MachineKey.Encode(plaintextBytes, MachineKeyProtection.All);
+        }
+
+        public static string Decrypt(string encryptedValue)
+        {
+            try
+            {
+                var decryptedBytes = MachineKey.Decode(encryptedValue, MachineKeyProtection.All);
+                return Encoding.UTF8.GetString(decryptedBytes);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static string generateRandom()
+        {
+            Random rnd = new Random();
+            int myRandom = rnd.Next(45);
+            return myRandom.ToString();
+        }
+    }
+}
